d7139a6bb21eea4d675f84f8c6daba9c0b5f78ed
Ryan Logan <ryan.logan01@gmail.com>
2021-04-24 18:04:00 -0500

    refactor: major change to code
    change data structures to dictionary and double linked list.
    
    all test examples pass

--- proj2/problem1.py
+++ proj2/problem1.py
@@ -1,47 +1,81 @@
 """
-maps and Hash
-a dictionary can be used with this problem to facilitate constant, i.e. O(1), lookups insertions and deletions
-need a hash function
-using larger table sized would be ideal for a production implementation. For this project we will use a Prime
-value of 7 for a smaller container lookup. However 31 would be better. This implementation doesn't take into account collisions
-the test data should limit collision so this was not worked on as none of the test cases require this.
+maps and double linked list
+a double linked list can be used as a LRU cache
+a dictionary can be used to map the keys to the nodes to facilitate constant, i.e. O(1), lookups insertions and deletions
+added print lines to the cache lookups to see value in console.
+
 """
+
+class Node():
+    def __init__(self, key=0, value=0):
+        self.next = None
+        self.prev = None
+        self.value = value
+        self.key = key
 
 class LRU_Cache(object):
 
     def __init__(self, capacity):
         # Initialize class variables
-        self.size = capacity
-        self.table = [None]*1000
-        self.p = 11
+        self.maxsize = capacity
+        self.map = dict() #should be a map of the key and the node for the value
+        self.head = None
+        self.tail = None
 
         pass
 
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent.
-        hv = self._create_hash_value(key)
+        cache_map = self.map
+        cache_node = cache_map.get(key)
+        if self.head == cache_node:
+            return cache_node.value
+        if cache_node != None:
+            if self.tail != cache_node:
+                nxt = cache_node.next
+                if nxt.prev != None:
+                    nxt.prev = cache_node.prev
+            elif self.tail == cache_node:
+                self.tail = cache_node.prev
+            prev = cache_node.prev
+            if prev.next != None:
+                prev.next = cache_node.next
+            head = self.head
+            head.prev = cache_node            
+            cache_node.next = head
+            cache_node.prev = None
+            self.head = cache_node
 
-        if self.table[hv] != None:
-            return self.table[hv]
+            return cache_node.value
 
         return -1
         pass
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
-        hv = self._create_hash_value(key)
-        self.table[hv] = value
-        pass
+        cache_map = self.map
+        cache_item = Node(key, value)
+        if self.head == None:
+            self.head = cache_item
+            self.tail = cache_item
+        else:
+            if len(cache_map) == self.maxsize:
+                tail = self.tail
+                cache_map.pop(tail.key)
+                self.tail = tail.prev
 
-    def _create_hash_value(self, key): 
-        # create simple hash function from the key to create new key for Cache table
-        key = str(key)
-        hv = 0
-        current_coefficient = 1
-        for char in key: ## for loop handles if key is longer than 1 digit
-            hv += ord(key) * current_coefficient
-            current_coefficient *= self.p
-        return hv 
+            cache_item.next = self.head
+            head = self.head
+            head.prev = cache_item
+            self.head = cache_item
+
+        cache_map[key] = cache_item
+        print(cache_item)
+        print(cache_item.value)
+        print(cache_item.next)
+        print(cache_item.prev)
+        
+        pass
 
 our_cache = LRU_Cache(5)
 
@@ -50,12 +84,12 @@ our_cache.set(2, 2)
 our_cache.set(3, 3)
 our_cache.set(4, 4)
 
-
-our_cache.get(1)       # returns 1
-our_cache.get(2)       # returns 2
-our_cache.get(9)      # returns -1 because 9 is not present in the cache
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))     # returns -1 because 9 is not present in the cache
 
 our_cache.set(5, 5) 
 our_cache.set(6, 6)
 
-our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+print(our_cache.get(6))
+print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
